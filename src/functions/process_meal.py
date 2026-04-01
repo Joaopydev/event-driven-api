bcd66fd1e246adf3bb3c306cd154f1c2542fa459
Joao <joao.ribeiro@e-deploy.com.br>
2026-04-01 16:49:01 -0300

    implement process meal function

---
+++ src/functions/process_meal.py
@@ -0,0 +1,18 @@
+import json
+import asyncio
+
+from ..queues.process_meal import ProcessMeal
+from ..repository.meal_repository import MealRepository
+
+
+async def async_handler(event):
+    process_meal = ProcessMeal(MealRepository())
+    tasks = [
+        process_meal.process(file_key=json.loads(record["body"])["file_key"])
+        for record in event["Records"]
+    ]
+    await asyncio.gather(*tasks)
+
+
+def handler(event, _):
+    asyncio.run(async_handler(event=event))
