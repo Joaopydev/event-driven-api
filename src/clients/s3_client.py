7ab6ec343926397dce8106d176dd4c1ed760e457
Joao <joao.ribeiro@e-deploy.com.br>
2026-04-28 12:41:31 -0300

    minor changes

--- src/clients/s3_client.py
+++ src/clients/s3_client.py
@@ -1,5 +1,9 @@
 import aioboto3
+import boto3
 
-def get_s3_client():
+def get_async_s3_client():
     session = aioboto3.Session()
     return session.client("s3")
+
+sync_s3_client = boto3.client("s3")
+
--- src/services/storage.py
+++ src/services/storage.py
@@ -1,11 +1,10 @@
 import os
-from typing import Dict, Any
 from dotenv import load_dotenv
 
 from aiobotocore.response import StreamingBody
 from botocore.exceptions import ClientError
 
-from ..clients.s3_client import get_s3_client
+from ..clients.s3_client import get_async_s3_client, sync_s3_client
 
 
 load_dotenv()
@@ -14,50 +13,49 @@ load_dotenv()
 class StorageService:
 
     @classmethod
-    async def get_download_url(cls, file_key: str, expires_in: int = 300) -> str:
+    def get_download_url(cls, file_key: str, expires_in: int = 300) -> str:
         """To obtain a pre-signed URL for download, no content type parameters are required."""
         params = {
             "Bucket": os.getenv("BUCKET_NAME"),
             "Key": file_key,
         }
-        return await cls._get_presigned_url(
+        return cls._get_presigned_url(
             method_type="get_object",
             params=params,
             expires_in=expires_in,
         )
     
     @classmethod
-    async def get_upload_url(cls, file_key: str, content_type: str, expires_in: int = 600) -> str:
+    def get_upload_url(cls, file_key: str, content_type: str, expires_in: int = 600) -> str:
         """Content-Type is required to get pre-signed url for upload"""
         params = {
             "Bucket": os.getenv("BUCKET_NAME"),
             "Key": file_key,
             "ContentType": content_type
         }
-        return await cls._get_presigned_url(
+        return cls._get_presigned_url(
             method_type="put_object",
             params=params,
             expires_in=expires_in,
         )
 
     @classmethod
-    async def _get_presigned_url(cls, method_type: str, params: dict,  expires_in: int) -> str:
+    def _get_presigned_url(cls, method_type: str, params: dict,  expires_in: int) -> str:
         # Generate pre-signed URL
         try:
-            async with get_s3_client() as s3_client:
-                presigned_url = s3_client.generate_presigned_url(
-                    method_type,
-                    Params=params,
-                    ExpiresIn=expires_in,
-                )
-                return presigned_url
+            presigned_url = sync_s3_client.generate_presigned_url(
+                method_type,
+                Params=params,
+                ExpiresIn=expires_in,
+            )
+            return presigned_url
         except ClientError as e:
             raise RuntimeError(f"Error generating presigned URL: {e}") from e
     
     @classmethod
-    async def read_object_content(cls, key: str) -> Dict[str, Any]:
+    async def read_object_content(cls, key: str) -> bytes:
         try:
-            async with get_s3_client() as s3_client:
+            async with get_async_s3_client() as s3_client:
                 obj = await s3_client.get_object(
                     Bucket=os.getenv("BUCKET_NAME"),
                     Key=key
