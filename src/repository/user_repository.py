c64d46b981bffb61fca0255f26ba9276ff6d8c7c
Joao <joao.ribeiro@e-deploy.com.br>
2026-05-13 12:33:21 -0300

    update user repository

--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -1,6 +1,7 @@
+from datetime import date
 from sqlalchemy import select
 
-from ..db.models.users import User
+from ..db.models.users import User, GoalType, GenderType
 
 
 class UserRepository:
@@ -8,9 +9,39 @@ class UserRepository:
     def __init__(self, db_session):
         self.db_session = db_session
 
-    async def insert_user(self, name: str, email: str, password: str) -> User:
+    async def insert_user(
+        self,
+        name: str,
+        email: str,
+        password: str,
+        goal: GoalType,
+        gender: GenderType,
+        birth_date: date,
+        height: int,
+        weight: int,
+        activity_level: int,
+        calories: int,
+        proteins: int,
+        carbohydrates: int,
+        fats: int
+
+    ) -> User:
         async with self.db_session() as db:
-            user = User(name=name, email=email, password=password)
+            user = User(
+                name=name,
+                email=email,
+                password=password,
+                goal=goal,
+                gender=gender,
+                birth_date=birth_date,
+                height=height,
+                weight=weight,
+                activity_level=activity_level,
+                calories=calories,
+                proteins=proteins,
+                carbohydrates=carbohydrates,
+                fats=fats,
+            )
             db.add(user)
             await db.commit()
             await db.refresh(user)
